586afe03849b6af37ebb757ecf5021d3cca4a06d
Werten101 <alexanderkrikunov1@gmail.com>
2017-04-04 21:20:29 -0400

    jquery links added

---
+++ assets/js/js.js
@@ -0,0 +1,8 @@
+var apiUrl = "https://www.googleapis.com/civicinfo/v2/elections?key=<AIzaSyCwWAipG8Nje7KLxb1DP0d7ONK4gyFuxgo>"
+
+
+
+$.ajax({
+            url: apiUrl,
+            method: "GET"
+        })
