276387cec202d39d4b0f779c22df417caad711a7
Werten101 <alexanderkrikunov1@gmail.com>
2017-04-07 14:29:00 -0400

    new design and working first API displaying on all pages for now

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,22 +1,20 @@
-var queryUrl = "https://www.googleapis.com/civicinfo/v2/elections5";
+$.ajax({
+        method: 'GET',
+        url: 'https://raw.githubusercontent.com/unitedstates/congress-legislators/master/legislators-current.yaml'
+    })
+    .done(function(yamlResponse) {
+        var response = YAML.parse(yamlResponse);
+        // filter by gender
+        //var legislators = response.filter(x => x.bio.gender === 'M');
+           
+        //filter by state from last term only
+        var legislators = response.filter(x => x.terms[x.terms.length - 1].state === 'FL');
+
+        console.log(response);
 
 
+        legislators.forEach(function(x) {
+            $('body').append("<div>" + x.name.official_full + ": " + x.terms[x.terms.length - 1].state + "</div>")
+        });
 
-
-$("#addGif").on("click", function(e){
-	e.preventDefault();
-console.log("click");
-
-	$.ajax({
-            url: queryUrl,
-            method: "GET",
-            data: {
-            	key: "AIzaSyCwWAipG8Nje7KLxb1DP0d7ONK4gyFuxgo"
-            }
-        })
-			.done(function(response){
-				console.log(response);
-			});
-
-
-});
+    });
