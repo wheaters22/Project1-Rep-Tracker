8308347712fbb35d9666ef26cc0b1e571ec014e1
Werten101 <alexanderkrikunov1@gmail.com>
2017-04-08 04:19:19 -0400

    functional at some point, will have to do

--- assets/js/js.js
+++ assets/js/js.js
@@ -30,24 +30,46 @@ $.ajax({
 
         console.log(response1);
 
+        $("#newInfoHere").empty();
+        $("#newInfoHere").append("<h1> Hi "+firstNameInput+"! here these are the current legislators in your state!</h1>")
+
 
 
 
         legislators.forEach(function(x) {
-            $('body').append("<div>" + x.name.official_full + ": " + x.terms[x.terms.length - 1].state + "</div>")
+            $("#newInfoHere").append("<div>" + x.name.official_full + ": " + x.terms[x.terms.length - 1].state + "</div>")
 
         });
 
+        $("#newInfoHere").append("<h3> More info on them will be available soon! </h3><br><h3> We will E-mail you at "+emailInput+" when more info becomes available!</h3>");
+
     });
 
-    $.ajax({
-        dataType: 'json',
-        type: 'POST',
-        'Api-User-Agent': "Alex1 ('alexanderkrikunov1@mgail.com')",
-        method: 'GET',
-        url: 'https://en.wikipedia.org/w/api.php?action=query&titles=' + "Apple" + '"&prop=revisions&rvprop=content&format=json'
-    })
-    .done(function(response2) {
-        console.log(response2);
-    });
+//atempt 3 with getjson: failed
+    // $.getJSON( "https://en.wikipedia.org/w/api.php?action=query&titles=" + "Apple" + "&prop=revisions&rvprop=content", function( response2 ) {
+    //     console.log(response2);
+    // });
+
+//ajax atempt 2: failed
+//     $.ajax({
+//   dataType: "json",
+//   header: 'Access-Control-Allow-Origin',
+//   url: 'https://en.wikipedia.org/w/api.php?action=query&titles=' + "Apple" + '"&prop=revisions&rvprop=content&format=json',
+//   data: 'string',
+//   success: console.log("Yippie!")
+// })
+//     .done(function(response2){
+//         console.log(response2);
+//     });
+//ajax atempt 1: failed
+    // $.ajax({
+    //     dataType: 'json',
+    //     type: 'POST',
+    //     'Api-User-Agent': "Alex1 ('alexanderkrikunov1@mgail.com')",
+    //     method: 'GET',
+    //     url: 'https://en.wikipedia.org/w/api.php?action=query&titles=' + "Apple" + '"&prop=revisions&rvprop=content&format=json'
+    // })
+    // .done(function(response2) {
+    //     console.log(response2);
+    // });
 });
